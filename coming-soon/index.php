85694a857bb578c96cb69a4ad04b4d8873ebd43c
helen-hat <helen.snowfia@hashagile.com>
2024-10-08 13:44:50 +0530

    Initial commit of WordPress source code

---
+++ coming-soon/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Hash Agile Technologies</title>
+<meta name="description" content="For Decisive Agile Solutions | Hash Agile Technologies">
+<meta name="keywords" content="Hash Agile Technologies">
+<link href="tools/style.css" rel="stylesheet" type="text/css" media="screen" />
+<!--link href="tools/font-awesome/css/font-awesome.css" type="text/css" rel="stylesheet"-->
+<script type="text/javascript" src="tools/jquery.min.js"></script> 
+<link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+
+
+</head>
+<body>
+<div class="header">
+	<h1 id="logo">Hash Agile Technologies<span>For decisive agile solutions</span></h1>
+	<div class="seprator"></div>
+</div>
+
+<div class="content">
+		<p>Our website is coming soon!</p>
+		<p id="small">We're working hard on our new website to give you the best service.<br/>
+		Good things take time, great things take a little longer.</p>
+</div>
+
+
+<div id="counter">
+</div>
+
+
+
+<div class="social">
+		<div class="seprator"></div>
+
+<p>For any queries, contact at:
+<a href="mailto:hashagile.technologies@gmail.com">hashagile.technologies@gmail.com</a>
+
+	</ul>
+</div>
+
+	<script type="text/javascript" src="tools/jquery.countdown.min.js"></script>
+		<script>
+    jQuery(document).ready(function() {
+			/* Change the launch date according to the date you want to set.
+			Please take note that months ranges from 0-11 like an array setup. 
+			*/
+        var launchdate = new Date(2022, 10 - 1, 1);
+        jQuery('#counter').countdown({
+            until: launchdate
+        });
+    });
+	</script>
+
+
+
+</body>
+</html>
